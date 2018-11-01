9387b14b8489156b206aaa2fd2d0a80dfd42bb9e
sittikorn.c <sittikorn.c@motiftech.com>
2018-11-01 12:39:08 +0700

    Initial Branch

---
+++ src/main/java/th/co/motif/TrainingSpringBootMtApplication.java
@@ -0,0 +1,12 @@
+package th.co.motif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringBootMtApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrainingSpringBootMtApplication.class, args);
+  }
+}
